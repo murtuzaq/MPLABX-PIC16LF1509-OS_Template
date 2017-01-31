5f79b8d3388e320411c12cc76c4ad690a5ddb82b
murtuzaq <murtuzaq@gmail.com>
2017-01-31 11:00:42 -0500

    Initial Project CheckIn
    CheckIn MPLABX default project files as well as a  main.c and
    RevHistory.c

---
+++ project.X/RevHistory.c
@@ -0,0 +1,11 @@
+const char* Version = "Firmware Version 0.0.1";
+
+
+#if 0
+
+/* Rev History */
+//
+//
+//initial release;
+//
+#endif
---
+++ project.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: mquaizar
+ *
+ * Created on January 31, 2017, 10:30 AM
+ */
+
+
+#include <pic.h>
+
+void main(void) {
+    return;
+}
